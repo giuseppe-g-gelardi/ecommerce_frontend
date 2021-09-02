68c86924ec9a86b4b4565d5c6d2e6ae842396648
n-rdy <giuseppegelardi@icloud.com>
2021-09-02 10:11:00 -0500

    first commit

---
+++ ecommerce_project/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
+        <h1>ecommerce app.jsx</h1>
+      </div>
+     );
+  }
+}
+ 
+export default App;
