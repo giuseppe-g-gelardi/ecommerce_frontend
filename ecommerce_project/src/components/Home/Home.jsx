2f06c672a41e27478b29a1df3fbaba1f4110e7b3
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-07 16:53:49 -0500

    Changed the title and centered it and gave it color

--- ecommerce_project/src/components/Home/Home.jsx
+++ ecommerce_project/src/components/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import Homestyle from './Home.css'
 
 function Home() {
   return (
     <div className="Home">
+      <link rel="stylesheet" href={Homestyle}/>
       <div class="container">
         <div class="row">
-          <div class="col-md-6">
-          <h1>Welcome Home!</h1>
-          <span>please work</span>
-          <p>it worked!</p>
+          <h1 id="Homepageh1" class="text-center">Available Foods!</h1>
+        </div>
+        <div class="row">
+          <div class="col-md-6 secondsection">
+            <span>please work</span>
+            <p>it worked!</p>
           </div>
           <div class="col-md-6"></div>
           <div class="col-md-6"></div>
-          <div class="col-md-6">
+          <div class="col-md-6 secondsection">
             <h2>
               we sell food and stuff here bruh
             </h2>
@@ -21,15 +25,15 @@ function Home() {
               <div class="col-md-6">
                 <h2>
                   buy the food stuffs from us lmao
-                  </h2>
-                </div>
+                </h2>
+              </div>
               <div class="col-md-6"></div>
               <div class="col-md-6"></div>
               <div class="col-md-6">
                 <span>
                   plz i have bills
                 </span>
-                </div>
+              </div>
             </div>
         </div>
       </div>
