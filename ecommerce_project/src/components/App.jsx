5e52f7a0adeca7a7c919090fcaa5b69fc777c4ae
n-rdy <giuseppegelardi@icloud.com>
2021-09-07 09:56:14 -0500

    completely redoing the registration and login pages

--- ecommerce_project/src/components/App.jsx
+++ ecommerce_project/src/components/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './App.css';
-import Form from './Form/Form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Registration from './Registration/Registration';
+import Login from './Login/Login';
+
 
 function App() {
-  return <Form />;
+  return (
+    <div className="App">
+      <Router>
+        <div className="navbar">
+          <Link to='/login'>Login</Link>
+          <Link to='/registration'>Registration</Link>
+        </div>
+        <Switch>
+          <Route path='/registration' exact component={Registration}></Route>
+          <Route path='/Login' exact component={Login}></Route>
+
+        </Switch>
+      </Router>
+
+    </div>
+    );
 }
 
 export default App;
--- ecommerce_project/src/index.js
+++ ecommerce_project/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import App from './components/App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-reactDom.render(<App />, document.getElementById('root'))
+reactDom.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+document.getElementById('root'))
+
+// reactDom.render(<App />, document.getElementById('root'))
