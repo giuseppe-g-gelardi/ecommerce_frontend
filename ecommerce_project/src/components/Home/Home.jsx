fa7a71e394ddf9804480b255d843f41e65e9e16a
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-07 17:01:19 -0500

    Made changes to home page and resolving merge conflict

--- ecommerce_project/src/components/Home/Home.jsx
+++ ecommerce_project/src/components/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
 import Homestyle from './Home.css'
-=======
 import cheetah from './cheetah.png'
->>>>>>> 7729b3905164712c0ae5504c6c8063e8b4ad70c1
 
 function Home() {
   return (
     <div className="Home">
-<<<<<<< HEAD
       <link rel="stylesheet" href={Homestyle}/>
       <div class="container">
         <div class="row">
@@ -18,15 +14,6 @@ function Home() {
           <div class="col-md-6 secondsection">
             <span>please work</span>
             <p>it worked!</p>
-=======
-
-
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-          <h1>Welcome Home!</h1>
-          <span>please work</span>
->>>>>>> 7729b3905164712c0ae5504c6c8063e8b4ad70c1
           </div>
           <div class="col-md-6"></div>
             <img src={cheetah} alt='no-alt' />
