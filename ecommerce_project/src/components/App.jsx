9615cf6d5f328b4764a6644532a1cd5d1283a1ff
n-rdy <giuseppegelardi@icloud.com>
2021-09-07 15:16:56 -0500

    added a photo of a cheetah cus theyre so cute

--- ecommerce_project/src/components/App.jsx
+++ ecommerce_project/src/components/App.jsx
@@ -40,7 +40,6 @@ function App() {
           <Route path='/registration' exact component={Registration}></Route>
         </Switch>
       </Router>
-
     </div>
     );
 }
--- ecommerce_project/src/components/Home/Home.jsx
+++ ecommerce_project/src/components/Home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import cheetah from './cheetah.png'
 
 function Home() {
   return (
     <div className="Home">
+
+
       <div class="container">
         <div class="row">
           <div class="col-md-6">
           <h1>Welcome Home!</h1>
           <span>please work</span>
-          <p>it worked!</p>
+          <img src={cheetah} alt='no-alt' />
+
           </div>
           <div class="col-md-6"></div>
           <div class="col-md-6"></div>
