c27136246ae10d8c30c963c3ef70d25da492bb36
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-07 21:46:06 -0500

    Changed color to navbar links

--- ecommerce_project/src/components/App.jsx
+++ ecommerce_project/src/components/App.jsx
@@ -30,9 +30,9 @@ function App() {
         <div className="navbar">
           <Navbar id='navbar' fixed='top' bg='dark' variant='dark'>
             <Navbar.Brand href=''>Fooooooooood!</Navbar.Brand>
-              <Nav.Link href='/home'> Home</Nav.Link>
-              <Nav.Link href='/login'> Login</Nav.Link>
-              <Nav.Link href='/registration'> Register</Nav.Link>
+              <Nav.Link id="NavLinks" href='/home'> Home</Nav.Link>
+              <Nav.Link id="NavLinks" href='/login'> Login</Nav.Link>
+              <Nav.Link id="NavLinks" href='/registration'> Register</Nav.Link>
               <Nav className="ml-auto">
                 <SearchBar />
               </Nav>
