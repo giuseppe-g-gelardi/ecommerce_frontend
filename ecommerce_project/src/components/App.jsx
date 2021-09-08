f72b001c79d85a15cabf8b3431defa74bc4c31c8
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-08 01:01:06 -0400

    Fixed a space with Home Route

--- ecommerce_project/src/components/App.jsx
+++ ecommerce_project/src/components/App.jsx
@@ -21,14 +21,10 @@ import Home from './Home/Home';
 import Login from './Login/Login';
 import Registration from './Registration/Registration';
 import SearchBar from './SearchBar/SearchBar';
-<<<<<<< HEAD
-import Products from './Products/Products';
-=======
 import Account from './Account/Account';
 import Cart from './Cart/Cart';
 import AddProducts from './AddProducts/AddProducts';
 
->>>>>>> 833d4d73c6c65b9d4e2d6c14e5f8b9cfca55a9b0
 
 function App() {
   return (
@@ -41,15 +37,6 @@ function App() {
         <div className="navbar">
           <Navbar id='navbar' fixed='top' bg='dark' variant='dark'>
             <Navbar.Brand id="PageTitle" href=''>Fooooooooood!</Navbar.Brand>
-<<<<<<< HEAD
-            <Nav.Link id="NavLinks" href='/home'> Home</Nav.Link>
-            <Nav.Link id="NavLinks" href='/login'> Login</Nav.Link>
-            <Nav.Link id="NavLinks" href='/registration'> Register</Nav.Link>
-            <Nav.Link id="NavLinks" href='/User'> User</Nav.Link>
-            <Nav className="ml-auto">
-              <SearchBar />
-            </Nav>
-=======
               <Nav.Link id="NavLinks" href='/home'> Home</Nav.Link>
               <Nav.Link id="NavLinks" href='/login'> Login</Nav.Link>
               <Nav.Link id="NavLinks" href='/registration'> Register</Nav.Link>
@@ -58,11 +45,10 @@ function App() {
               <Nav className="ml-auto">
                 <SearchBar />
               </Nav>
->>>>>>> 833d4d73c6c65b9d4e2d6c14e5f8b9cfca55a9b0
           </Navbar>
         </div>
         <Switch>
-          <Route path='/home' exact component={Home}> </Route>
+          <Route path='/home' exact component={Home}></Route>
           <Route path='/login' exact component={Login}></Route>
           <Route path='/registration' exact component={Registration}></Route>
           <Route path='/Account' exact component={Account}></Route>
